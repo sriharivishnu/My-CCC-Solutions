b7a2bb97bc345ccc54f3e30590103f91b5851204
Srihari Vishnu <37857112+sriharivishnu@users.noreply.github.com>
2019-01-22 22:31:33 -0500

    Create ccc2017s4.h

---
+++ ccc2017s4.h
@@ -0,0 +1,39 @@
+// Solution by Srihari Vishnu
+// Starting from bottom and building up
+// Noticing the value at each node is the sum of all the nodes
+// It leads to
+#include <iostream>
+#include <stack>
+#include <string>
+#include <vector>
+#include <queue>
+using namespace std;
+vector<int> hill[9999];
+int counters[9999];
+int n;
+void init() {
+  for (int i= 0; i <= n; i++) {
+    counters[i] = 0;
+  }
+}
+int main() {
+  cin >> n;
+  int x,y;
+  cin >> x >> y;
+  while (x != 0 && y != 0) {
+    hill[y].push_back(x);
+    cin >> x >> y;
+  }
+  init ();
+
+  //Start Search
+  counters[n] = 1;
+  for (int i = n; i>0; i--) {
+    for (int j=0; j < hill[i].size(); j++) {
+      counters[hill[i][j]] += counters[i];
+    }
+  }
+  
+  cout << counters[1];
+
+}
