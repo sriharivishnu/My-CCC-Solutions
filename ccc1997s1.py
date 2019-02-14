4c1a4e268cf76630555ed492d8fde07bd0600e30
Srihari Vishnu <37857112+sriharivishnu@users.noreply.github.com>
2019-02-13 19:56:52 -0500

    Create ccc1997s1.py

---
+++ ccc1997s1.py
@@ -0,0 +1,20 @@
+for a0 in range(input()):
+  s = input()
+  v = input()
+  o = input()
+  subjects = []
+  verbs = []
+  objects = []
+
+  for x in range(s):
+    subjects.append(raw_input())
+  for x in range(v):
+    verbs.append(raw_input())
+  for x in range(o):
+    objects.append(raw_input())
+  
+  for x in range(s):
+    for y in range(v):
+      for z in range(o):
+        print subjects[x]+" "+verbs[y]+" "+objects[z]
+  
