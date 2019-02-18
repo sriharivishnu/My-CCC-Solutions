6b4c0ab8a2558f5171245bcf12698084b9399a45
Srihari Vishnu <37857112+sriharivishnu@users.noreply.github.com>
2019-02-18 15:23:24 -0500

    Create ccc2018s1.py

---
+++ ccc2018s1.py
@@ -0,0 +1,12 @@
+n = input()
+ar = []
+neighbour = [0.0 for x in range(n)]
+neighbour[0] = 10**10
+neighbour[n-1] = 10**10
+for x in range(n):
+  ar.append(input())
+
+ar.sort()
+for x in range(1,n-1):
+  neighbour[x] = float(ar[x]-ar[x-1])/2.0 + float(ar[x+1]-ar[x])/2.0
+print min(neighbour)
