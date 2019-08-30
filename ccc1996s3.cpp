009786fb8c135ca65beb9f09ebe56fa0cef3a3f9
Srihari Vishnu <srihari.vishnu@gmail.com>
2019-08-30 10:58:18 -0400

    Update ccc1996s3.cpp

--- ccc1996s3.cpp
+++ ccc1996s3.cpp
@@ -1,5 +1,7 @@
 //Solution by Srihari Vishnu
-//Drawing out a string tree helps to visualize
+//CCC 1996 S3
+//Drawing out string tree helps to visualize
+
 #include <iostream>
 #include <string>
 #include <stdio.h>
@@ -11,6 +13,7 @@ void findPattern(string str, int ones, int zeroes) {
     return;
   }
   string temp = str;
+
   if (ones > 0) {
     findPattern(str.append("1"),ones-1,zeroes);
   }
@@ -24,7 +27,9 @@ int main() {
   cin >> a;
   for (int i =0; i<a; i++) {
     cin >> n >> k;
+    cout << "The bit patterns are" << endl;
     findPattern("", k, n-k);
+    cout << endl;
   }
   return 0;
 }
